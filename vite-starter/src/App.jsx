b22d37a129db5ad1d89ca84c69b4b6c0687c4534
kiyoungsong <thdrldud369@gmail.com>
2024-05-30 16:25:25 +0900

    ✨ [feat]: 메인 컴포넌트 추가

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [buttonColor, setButtonColor] = useState("medium-violet-red");
+  const nextColor =
+    buttonColor === "medium-violet-red" ? "midnight-blue" : "medium-violet-red";
+  const [isChecked, setChecked] = useState(false);
+
+  const handleClick = (e) => {
+    const { checked } = e.target;
+    setChecked(checked);
+  };
+
   return (
     <div>
-      <h1>I'm gonna learn React Testing Library</h1>
+      {/* <h1>I'm gonna learn React Testing Library</h1> */}
+      <button
+        className={`${isChecked ? "gray" : buttonColor}`}
+        onClick={() => setButtonColor(nextColor)}
+        disabled={isChecked}
+      >
+        Change to {nextColor}
+      </button>
+      <br />
+      <input
+        type="checkbox"
+        id="disable-button-checkbox"
+        checked={isChecked}
+        onChange={handleClick}
+      />
+      <label htmlFor="disable-button-checkbox">Disable button</label>
     </div>
   );
 }
