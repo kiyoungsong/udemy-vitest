865cb82a72830a0e737f9a1d245a61cbd6f9ef08
ky.song <thdrldud369@gmail.com>
2024-06-04 13:24:27 +0900

    🚩 [Feat]: 렌더링 확인을 위한 Options 적용

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,9 +1,13 @@
+import Options from "./page/entry/Options";
 import { SummaryForm } from "./page/summary/SummaryForm";
 
 function App() {
   return (
     <div>
       <h1>Sundaes on Demand</h1>
+
+      <Options optionType={"scoops"} />
+      <Options optionType={"toppings"} />
       <SummaryForm />
     </div>
   );
