dacf282c84fd63a0bcd397bcf67e566659def689
ky.song <thdrldud369@gmail.com>
2024-06-04 13:23:13 +0900

    ✅ [Feat]: popover 테스트 및 컴포넌트 추가

--- sundae-starter/src/page/tests/SummaryForm.test.jsx
+++ sundae-starter/src/page/tests/SummaryForm.test.jsx
@@ -39,10 +39,23 @@ test("popover", async () => {
   const user = userEvent.setup();
 
   render(<SummaryForm />);
-
   //popover starts out hidden
-  const popoverElement =
-    // 체크박스 라벨에 마우스 가져다 대면 popover
-    await user.hover();
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+
+  // null 체크
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // 체크박스 라벨에 마우스 가져다 대면 popover
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsAndConditions);
+
+  // get을사용해서 팝업이 있을것으로 가정해야하니 get을 사용해서 가져옴
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
   // 체크박스 라벨에 마우스를 빼면 popover 사라짐
+  await user.unhover(termsAndConditions);
+  expect(popover).not.toBeInTheDocument();
 });
