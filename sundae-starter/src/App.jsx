72e3c9a5c4dd4c7f8ba98765b1fec239404825c1
kiyoungsong <thdrldud369@gmail.com>
2024-06-10 16:19:31 +0900

    🚧 [Feat]: App 컴포넌트 처리중

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,12 +1,28 @@
 import Container from "react-bootstrap/Container";
 import OrderEntry from "./page/entry/OrderEntry";
 import { OrderDetailsProvider } from "./contexts/OrderDetails";
+import { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState("inProgress");
+
+  let Component = OrderEntry;
+
+  switch (status) {
+    case "inProgress":
+      Component = OrderEntry;
+      break;
+    case "review":
+      Component = OrderEntry;
+      break;
+    case "completed":
+      Component = OrderEntry;
+  }
+
   return (
     <Container>
       <OrderDetailsProvider>
-        <OrderEntry />
+        <Component setStatus={setStatus} />
       </OrderDetailsProvider>
     </Container>
   );
